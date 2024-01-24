adabca060e42404b7a5516b95675257468b55f52
ivanovdale <dania-spb@mail.ru>
2024-01-24 18:23:57 +0300

    Initial Commit

---
+++ iOS12-HW12-Daniil-Ivanov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS12-HW12-Daniil-Ivanov
+//
+//  Created by Daniil (work) on 24.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
